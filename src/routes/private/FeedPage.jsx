e7d970324e605ef0a835cfe9087a4fdab08e289a
mustafadede <mustafa.dede.0016@gmail.com>
2023-07-20 11:34:16 +0300

    navbar bg changes

--- src/routes/private/FeedPage.jsx
+++ src/routes/private/FeedPage.jsx
@@ -7,12 +7,12 @@ function FeedPage() {
   const user = getAuth().currentUser;
   return (
     <>
-      <Navbar isNotLoggedin={false} additionalClasses="sticky top-0 bg-cGradient2 z-30" />
+      <Navbar isNotLoggedin={false} additionalClasses="sticky top-0 bg-gradient-to-t from-transparent to-cGradient2 z-30" />
       <div className="mx-10 flex">
         <div className="hidden lg:w-1/4 h-fit lg:flex sticky top-[4.7rem] bg-cGradient2">
           <ProfileCard user={user} />
         </div>
-        <div className=" w-full lg:w-3/3 h-[200vh]"></div>
+        <div className=" w-full lg:w-3/3 h-[200vh] "></div>
         <div className="hidden w-full md:w-1/2 lg:w-1/3 h-fit lg:flex sticky top-[4.7rem] justify-center">
           <PopularCard />
         </div>
