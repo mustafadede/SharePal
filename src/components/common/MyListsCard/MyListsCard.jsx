36be7017b6e31f8eafda903221ba38bdbcf7354c
mustafadede <mustafa.dede.0016@gmail.com>
2023-08-07 21:22:39 +0300

    FeedPage animation2

--- src/components/common/MyListsCard/MyListsCard.jsx
+++ src/components/common/MyListsCard/MyListsCard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { PlusIcon } from "@radix-ui/react-icons";
+import { motion } from "framer-motion";
+
 function MyListsCard() {
   const clickHandler = () => {
     console.log("clicked");
   };
   return (
-    <div className="p-4 mt-4 w-72 h-fit bg-slate-900 rounded-2xl">
+    <motion.div
+      className="p-4 mt-4 w-72 h-fit bg-slate-900 rounded-2xl"
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
       <div className="flex justify-between">
         <p className="text-xl text-slate-200">My Lists</p>
         <button onClick={clickHandler}>
@@ -15,7 +22,7 @@ function MyListsCard() {
       <div className="flex flex-col justify-center pt-4 ">
         <p className="text-md text-slate-400">You have no lists yet.</p>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
