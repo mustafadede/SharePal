4705c78dd408c1f12f42f6b4333693fb20c98773
mustafadede <mustafa.dede.0016@gmail.com>
2024-06-12 19:17:02 +0300

    watchedThisModal ui fixed

--- src/components/common/watchedThis/ActionButton/ActionButton.jsx
+++ src/components/common/watchedThis/ActionButton/ActionButton.jsx
@@ -4,7 +4,7 @@ function ActionButton({ title, icon, onClick }) {
   return (
     <button className="flex items-center justify-center w-1/2 gap-2 p-2 text-lg rounded-lg text-cWhite bg-fuchsia-800" onClick={onClick}>
       {icon}
-      <p className="text-center text-slate-200">{title}</p>
+      <p className="text-sm text-center md:text-md text-slate-200">{title}</p>
     </button>
   );
 }
--- src/components/common/watchedThis/WatchedThisImages.jsx
+++ src/components/common/watchedThis/WatchedThisImages.jsx
@@ -8,14 +8,14 @@ function WatchedThisImages() {
       <img
         src={`https://image.tmdb.org/t/p/w500/${modalHasData?.backdrop}`}
         alt={modalHasData?.title}
-        className="absolute z-0 object-cover w-full rounded-lg opacity-30 h-inherit md:h-48"
+        className="absolute z-0 object-cover w-full rounded-lg h-36 md:h-48 opacity-30 h-inherit"
       />
       <img
         src={`https://image.tmdb.org/t/p/w500/${modalHasData?.poster}`}
         alt={modalHasData?.title}
-        className="z-10 w-12 h-32 mt-4 transition-colors duration-300 border rounded-lg border-slate-200 hover:border-fuchsia-600 md:w-20 md:h-28"
+        className="z-10 w-12 h-full mt-4 transition-colors duration-300 border rounded-lg border-slate-200 hover:border-fuchsia-600 md:w-20 md:h-28"
       />
-      <h1 className="z-10 text-xl font-bold text-center text-cWhite">{modalHasData?.title}</h1>
+      <h1 className="z-10 font-bold text-center text-md md:text-xl text-cWhite">{modalHasData?.title}</h1>
     </div>
   );
 }
