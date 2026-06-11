30b0f125e4ff071f5124b9941d6c039e9d39f2b7
Mustafa DEDE <95627279+mustafadede@users.noreply.github.com>
2026-06-11 20:48:15 +0300

    prod: vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["/node_modules/react-router-dom/index.js"],
-    },
-  },
 });
